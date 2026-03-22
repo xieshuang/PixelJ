8aec29fe7d34c80b6a9d54cedd69c675f1baea35
xieshuang <xieshuang405@gmail.com>
2026-03-22 20:11:41 +0800

    chore: 改用 Maven javafx:run 启动，脚本立即退出

--- run.bat
+++ run.bat
@@ -1,14 +1,4 @@
 @echo off
 cd /d %~dp0
-
-set JAR_PATH=target\pixelj-1.0-SNAPSHOT.jar
-
-if exist "%JAR_PATH%" (
-    echo Starting PixelJ...
-    start javaw -jar "%JAR_PATH%"
-) else (
-    echo Building PixelJ first...
-    call mvn package -q -DskipTests
-    start javaw -jar "%JAR_PATH%"
-)
+start cmd /c "mvn javafx:run"
 exit
